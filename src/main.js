1c69a57c45af5b1e5f6aeb985161b40e261bbeb2
Bryan Vaz <1505615-bryanvaz@users.noreply.gitlab.com>
2021-01-04 16:33:14 -0500

    add example with CSS modules

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import vueCustomElement from 'vue-custom-element';
 // import App from './App.vue';
 import StandaloneText from './components/StandaloneText.vue';
+import StyleModuleExample from './components/StyleModuleExample.vue';
 
 Vue.config.productionTip = false;
 
@@ -13,7 +14,6 @@ Vue.use(vueCustomElement);
 
 Vue.customElement('standalone-text', StandaloneText, {
   shadow: true,
-  // shadowCss: '.test-css { color: black; }',
   beforeCreateVueInstance(root) {
     const rootNode = root.el.getRootNode();
 
@@ -27,6 +27,22 @@ Vue.customElement('standalone-text', StandaloneText, {
       root.shadowRoot = document.head;
     }
 
+    return root;
+  },
+});
+
+Vue.customElement('style-module-example', StyleModuleExample, {
+  shadow: true,
+  beforeCreateVueInstance(root) {
+    const rootNode = root.el.getRootNode();
+
+    if (rootNode instanceof ShadowRoot) {
+      // eslint-disable-next-line no-param-reassign
+      root.shadowRoot = rootNode;
+    } else {
+      // eslint-disable-next-line no-param-reassign
+      root.shadowRoot = document.head;
+    }
     return root;
   },
 });
